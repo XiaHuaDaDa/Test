b4b9c5e620a26c06e7d057c26725156456a1e5e9
XiaHuaDaDa <236808110@qq.com>
2021-08-10 03:15:23 +0800

    header

---
+++ genshin/src/pages/Home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './components/HomeHeader.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    'home-header': HomeHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
